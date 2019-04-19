b31406bafd1a8dafaf08ab83b44434694a8b50e5
prashant21GITHUB <prashant.bajpai21@gmail.com>
2019-04-20 01:38:33 +0530

    added support for checking contact if it is registered or not

--- app/src/main/java/com/pyb/trackme/HomeActivity.java
+++ app/src/main/java/com/pyb/trackme/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.pyb.trackme;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,6 +12,7 @@ import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -62,12 +65,13 @@ import org.json.JSONObject;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private SearchView searchView;
     private ListView trackListView;
     private List<String> coordinatesList;
     private String loggedInName;
@@ -75,9 +79,6 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     private TextView greetingsView;
     private EditText mobileNumberToTrack;
     private Button trackBtn;
-
-    public static final int RequestPermissionCode  = 1 ;
-    public static final int EXIT_CODE = 111;
 
     private Socket mSocket;
     private Emitter.Listener onSocketConnect = new Emitter.Listener() {
@@ -89,6 +90,7 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     };
 
     private void onServerConnection() {
+        mSocket.emit("connectedMobile", loggedInMobile);
         runOnUiThread(new Runnable() {
 
             public void run() {
@@ -145,33 +147,11 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap map) {
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(-33.852, 151.211);
+        LatLng india = new LatLng(20.5937, 78.9629);
         googleMap = map;
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-//        mLocationRequest = new LocationRequest();
-//        mLocationRequest.setInterval(5000); // two minute interval
-//        mLocationRequest.setFastestInterval(120000);
-//        mLocationRequest.setSmallestDisplacement(1);
-//        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-//
-//        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            if (ContextCompat.checkSelfPermission(this,
-//                    Manifest.permission.ACCESS_FINE_LOCATION)
-//                    == PackageManager.PERMISSION_GRANTED) {
-//                //Location Permission already granted
-//                mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
-//                googleMap.setMyLocationEnabled(true);
-//            } else {
-//                //Request Location Permission
-//                checkLocationPermission();
-//            }
-//        }
-//        else {
-//            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
-//            googleMap.setMyLocationEnabled(true);
-//        }
+        googleMap.addMarker(new MarkerOptions().position(india)
+                .title("India"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(india));
 
     }
 
@@ -179,16 +159,11 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
         @Override
         public void onLocationResult(LocationResult locationResult) {
             List<Location> locationList = locationResult.getLocations();
-//            progressBar.setVisibility(View.INVISIBLE);
             if (locationList.size() > 0) {
                 //The last location in the list is the newest
                 Location location = locationList.get(locationList.size() - 1);
                 Log.i("MapsActivity", "Location: " + location.getLatitude() + " " + location.getLongitude());
                 mLastLocation = location;
-//                if (mCurrLocationMarker != null) {
-//                    mCurrLocationMarker.remove();
-//                }
-
                 //Place current location marker
                 LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
                 if(mCurrLocationMarker == null) {
@@ -199,7 +174,6 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
                     mCurrLocationMarker = googleMap.addMarker(markerOptions);
                 }
                 mCurrLocationMarker.setPosition(latLng);
-
                 //move map camera
                 googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
             }
@@ -258,12 +232,7 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
                     } catch (JSONException e) {
                         return;
                     }
-
-//                    arrayAdapter.addAll(lat, lng);
                     new UpdateCurrentTrackingPosition(lat, lng).invoke();
-
-                    // add the message to view
-//                    addMessage(username, message);
                 }
             });
         }
@@ -273,73 +242,6 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
         loggedInName = bundle.getString("name", "");
         loggedInMobile = bundle.getString("mobile", "");
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-//        EnableRuntimePermission();
-    }
-
-    public void GetContactsIntoArrayList(){
-
-        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null, null, null);
-        String name, number;
-        while (cursor.moveToNext()) {
-
-            name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-
-            number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-//            contactList.add(name + " "  + ":" + " " + number);
-        }
-
-        cursor.close();
-
-    }
-
-    public void EnableRuntimePermission(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, RequestPermissionCode);
-            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
-        }
-
-//        if (ActivityCompat.shouldShowRequestPermissionRationale(
-//                HomeActivity.this,
-//                Manifest.permission.READ_CONTACTS))
-//        {
-//
-//            Toast.makeText(HomeActivity.this,"CONTACTS permission allows us to Access contacts", Toast.LENGTH_LONG).show();
-//
-//        } else {
-//
-//            ActivityCompat.requestPermissions(HomeActivity.this,new String[]{
-//                    Manifest.permission.READ_CONTACTS}, RequestPermissionCode);
-//
-//        }
-    }
-
-//    @Override
-//    public void onRequestPermissionsResult(int RC, String per[], int[] PResult) {
-//
-//        switch (RC) {
-//
-//            case RequestPermissionCode:
-//
-//                if (PResult.length > 0 && PResult[0] == PackageManager.PERMISSION_GRANTED) {
-//                    searchView.setEnabled(true);
-//
-//                    Toast.makeText(HomeActivity.this,"Permission Granted, Now your application can access CONTACTS.", Toast.LENGTH_LONG).show();
-//
-//                } else {
-//
-//                    Toast.makeText(HomeActivity.this,"Permission Canceled, Now your application cannot access CONTACTS.", Toast.LENGTH_LONG).show();
-//                    this.finish();
-//
-//                }
-//                break;
-//        }
-//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -356,20 +258,28 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
             case R.id.logout:
                 clearSavedLoginDetails();
                 Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
-                startActivityForResult(intent, EXIT_CODE);
+                startActivity(intent);
                 return true;
-
+            case R.id.share_location:
+                Intent i = new Intent(HomeActivity.this, SelectContactsActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("name", loggedInName);
+                bundle.putString("mobile", loggedInMobile);
+                i.putExtras(bundle);
+                startActivity(i);
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
 
         }
     }
 
+
     private void clearSavedLoginDetails() {
         SharedPreferences preferences = getSharedPreferences(getApplicationInfo().packageName +"_Login", MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString("Mobile", "");
-        editor.putString("Password", "");
+        editor.putString("Name", "");
         editor.commit();
         this.loggedInName = "";
         this.loggedInMobile = "";
@@ -378,88 +288,12 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onDestroy() {
         super.onDestroy();
+        googleMap.clear();
         if(mSocket.connected()) {
             String mobileNo = mobileNumberToTrack.getText().toString();
             mSocket.disconnect();
             mSocket.off(mobileNo, onNewMessage);
         }
-    }
-
-    private void checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                new AlertDialog.Builder(this)
-                        .setTitle("Location Permission Needed")
-                        .setMessage("This app needs the Location permission, please accept to use location functionality")
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                //Prompt the user once explanation has been shown
-                                ActivityCompat.requestPermissions(HomeActivity.this,
-                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                        MY_PERMISSIONS_REQUEST_LOCATION );
-                            }
-                        })
-                        .create()
-                        .show();
-
-
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_LOCATION );
-            }
-        }
-    }
-
-    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // location-related task you need to do.
-                    if (ContextCompat.checkSelfPermission(this,
-                            Manifest.permission.ACCESS_FINE_LOCATION)
-                            == PackageManager.PERMISSION_GRANTED) {
-//
-//                        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
-//                        googleMap.setMyLocationEnabled(true);
-                    }
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        googleMap.clear();
     }
 
     private class UpdateCurrentTrackingPosition {
--- app/src/main/java/com/pyb/trackme/SelectContactsActivity.java
+++ app/src/main/java/com/pyb/trackme/SelectContactsActivity.java
@@ -1,6 +1,275 @@
 package com.pyb.trackme;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class ShareLocationActivity extends AppCompatActivity {
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+public class SelectContactsActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_PICK_CONTACT = 131;
+    private EditText inputContact;
+    private ListView contactsListView;
+    private ImageButton openContactsBtn;
+    private ImageButton addBtn;
+    private Button submitBtn;
+    private List<Pair<String, String>> contactsList;
+    private CustomListViewAdapter listViewAdapter;
+    private ProgressBar progressBar;
+    private String loggedInName;
+    private String loggedInMobile;
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.select_contacts_activity);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        inputContact = findViewById(R.id.selected_mobile_number);
+        contactsListView = findViewById(R.id.contacts_view);
+        openContactsBtn = findViewById(R.id.imageButton);
+        submitBtn = findViewById(R.id.done_button);
+        addBtn = findViewById(R.id.add_button);
+        progressBar = findViewById(R.id.select_contact_progressBar);
+        contactsList = new ArrayList<>();
+        attachListeners();
+        listViewAdapter = new CustomListViewAdapter(this, contactsList);
+        contactsListView.setAdapter(listViewAdapter);
+        contactsListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                showAlertDialog(position);
+                return true;
+            }
+        });
+        setLoggedInUserDetails(getIntent().getExtras());
+    }
+
+    private void setLoggedInUserDetails(Bundle bundle) {
+        loggedInName = bundle.getString("name", "");
+        loggedInMobile = bundle.getString("mobile", "");
+    }
+
+    private void showAlertDialog(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SelectContactsActivity.this)
+                .setTitle("Confirm")
+                .setMessage("Do not share location with " + contactsList.get(position).first +" ?")
+                .setCancelable(true)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        contactsList.remove(position);
+                        listViewAdapter.notifyDataSetChanged();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void attachListeners() {
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAddButtonClick();
+            }
+        });
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareLocation();
+
+            }
+        });
+        openContactsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickContact();
+            }
+        });
+    }
+
+    private void shareLocation() {
+        RequestParams requestParams = new RequestParams();
+        requestParams.setUseJsonStreamer(true);
+        List<String> contacts = new ArrayList<>(contactsList.size());
+        for(Pair<String, String> pair : contactsList) {
+            contacts.add(pair.second);
+        }
+        requestParams.put("mobile", loggedInMobile);
+        requestParams.put("contacts", contacts);
+        APIClient.put("user/location/share", requestParams, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                progressBar.setVisibility(View.GONE);
+                try {
+                    if(response.getBoolean("success")) {
+                        String[] contacts = new String[contactsList.size()];
+                        int i = 0;
+                        for(Pair<String, String> pair : contactsList) {
+                            contacts[i++] = pair.first;
+                        }
+                        AlertDialog.Builder builder = new AlertDialog.Builder(SelectContactsActivity.this)
+                                .setTitle("Shared location with")
+                                .setItems(contacts, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) { }
+                                })
+                                .setCancelable(true)
+                                .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                    }
+                                });
+                        AlertDialog alert = builder.create();
+                        alert.show();
+                    } else {
+                        Toast.makeText(SelectContactsActivity.this, "Failed to share location, please try after sometime !!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(SelectContactsActivity.this, "Internal error, please try after sometime !!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(SelectContactsActivity.this, "Internal error, please try after sometime !!", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+    private void onAddButtonClick() {
+        String number = inputContact.getText().toString();
+        if(ValidationUtils.isValidNumber(number)) {
+            addContactIfRegistered(number, number);
+        } else {
+            Toast.makeText(this, "Enter valid mobile number !!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void pickContact() {
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, REQUEST_CODE_PICK_CONTACT);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode)
+        {
+            case (REQUEST_CODE_PICK_CONTACT):
+                if (resultCode == Activity.RESULT_OK)
+                {
+                    Uri contactUri = data.getData();
+                    // We only need the NUMBER column, because there will be only one row in the result
+                    String[] projection = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                            ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                    // Perform the query on the contact to get the NUMBER column
+                    // We don't need a selection or sort order (there's only one result for the given URI)
+                    // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                    // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                    // Consider using CursorLoader to perform the query.
+                    Cursor cursor = getContentResolver()
+                            .query(contactUri, projection, null, null, null);
+                    cursor.moveToFirst();
+
+                    // Retrieve the phone number from the NUMBER column
+                    int columnName = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                    int columnNumber = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                    String name = cursor.getString(columnName);
+                    String number = cursor.getString(columnNumber);
+                    number = number.replace("-","");
+                    number = number.replace(" ","");
+                    if(number.length() > 10) {
+                        number = number.substring(number.length() - 10);
+                    }
+                    addContactIfRegistered(number, name);
+                }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void addContactIfRegistered(final String number, final String name) {
+        progressBar.setVisibility(View.VISIBLE);
+        RequestParams requestParams = new RequestParams();
+        requestParams.setUseJsonStreamer(true);
+        requestParams.put("mobile", number);
+        APIClient.put("user/isregistered", requestParams, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                progressBar.setVisibility(View.GONE);
+                try {
+                    if(response.getBoolean("success")) {
+                        contactsList.add(new Pair<>(name, number));
+                        listViewAdapter.notifyDataSetChanged();
+                        Toast.makeText(SelectContactsActivity.this, "Contact added to the list !!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(SelectContactsActivity.this, "Contact is not registered on TrackMe !!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                progressBar.setVisibility(View.GONE);
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 }
