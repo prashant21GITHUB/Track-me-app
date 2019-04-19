c9dba70e73da8fba784a05fb21581ef896b1172f
Prashant Bajpai <prashant.bajpai21@gmail.com>
2019-04-19 13:05:50 +0530

    added select multiple contact support

---
+++ app/src/main/java/com/pyb/trackme/CustomListViewAdapter.java
@@ -0,0 +1,4 @@
+package com.pyb.trackme;
+
+public class CustomListViewAdapter {
+}
---
+++ app/src/main/java/com/pyb/trackme/SelectContactsActivity.java
@@ -0,0 +1,6 @@
+package com.pyb.trackme;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ShareLocationActivity extends AppCompatActivity {
+}
